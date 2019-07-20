729b638e05e31015123c5b5a05a75910ffda7b40
i7 5820K <francofazzito7@gmail.com>
2019-07-20 16:33:10 -0300

    Finalizacion con clase 5

---
+++ Principio de Inversión de Dependencia/Principio de Inversión de Dependencia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principio_de_Inversión_de_Dependencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
