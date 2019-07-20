7a6578cd26e1fdfe570c721c4059bbb6d006519d
FrancoFazzito <48734991+FrancoFazzito@users.noreply.github.com>
2019-07-20 15:43:00 -0300

    Add files via upload

---
+++ Principio de sustitución de Liskov/Principio de sustitución de Liskov/Noticiero.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principio_de_sustitución_de_Liskov
+{
+    abstract class Principal
+    {
+       protected string mensaje;
+
+        public Principal(string mensaje)
+        {
+            this.mensaje = mensaje ?? throw new ArgumentNullException(nameof(mensaje));
+        }
+
+        public abstract void muestra();
+        //public void muestra()
+        //{
+        //    Console.ForegroundColor = ConsoleColor.Yellow;
+        //    Console.WriteLine($"noticiero: {mensaje}");
+        //}
+        //public virtual void muestra()
+        //{
+        //    Console.ForegroundColor = ConsoleColor.Yellow;
+        //    Console.WriteLine($"noticiero: {mensaje}");
+        //}
+    }
+    class Noticiero : Principal
+    {
+        public Noticiero(string mensaje) : base(mensaje)
+        {
+        }
+
+        public override void muestra()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"noticiero: {mensaje}");
+        }
+    }
+    class Radio : Principal
+    {
+        public Radio(string mensaje) : base(mensaje)
+        {
+        }
+
+        public override void muestra()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"radio: {mensaje}");
+        }
+
+        //reemplazo de muestra de clase padre
+        //public new void muestra()
+        //{
+        //    Console.ForegroundColor = ConsoleColor.Red;
+        //    Console.WriteLine($"radio: {mensaje}");
+        //}
+        //public override void muestra()
+        //{
+        //    Console.ForegroundColor = ConsoleColor.Red;
+        //    Console.WriteLine($"radio: {mensaje}");
+        //}
+    }
+}
---
+++ Principio de sustitución de Liskov/Principio de sustitución de Liskov/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principio_de_sustitución_de_Liskov
+{
+    class Program
+    {
+        private const string MENSAJE_A_MOSTRAR = "HOLA A TODOS";
+
+        static void Main(string[] args)
+        {
+            //objetos de clases hijas pueden reemplazar a las clases padres sin problemas
+            //garantiza el uso correcto del polimorfismo
+
+            //Noticiero noticiero = new Noticiero(MENSAJE_A_MOSTRAR);
+            //noticiero.muestra();
+
+            //Radio radio = new Radio(MENSAJE_A_MOSTRAR);
+            //radio.muestra();
+
+            //Noticiero sustitucion = new Radio(MENSAJE_A_MOSTRAR); //ACA SE MUESTRA EL COMPORTAMIENTO ERRONEO LOGICO
+            //sustitucion.muestra();
+
+            //PODREMOS UTILIZAR -> VIRTUAL PADRE Y OVERRIDE EN HIJA (UTILIZA COMPORTAMIENTO DE LA HIJA)
+            //PROBLEMA DE DISEÑO -> CUANDO HACEMOS OVERRIDE ESTAMOS PERDIENDO EL COMPORTAMIENTO DE LA CLASE PADRE
+
+            //PARA ESTO ENTRA LA SUSTITUCION DE LISKOV
+
+            Principal noticiero = new Noticiero(MENSAJE_A_MOSTRAR);
+            noticiero.muestra();
+
+            Principal radio = new Radio(MENSAJE_A_MOSTRAR);
+            radio.muestra();
+
+
+            Console.ReadLine();
+        }
+    }
+}
